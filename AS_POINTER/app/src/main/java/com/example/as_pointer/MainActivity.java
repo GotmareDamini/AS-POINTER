c01d306385e62b52bb1f31fbe807fbd508e97429
damini <daminigotmare1771@gmail.com>
2020-01-11 15:13:50 +0530

    damini

--- AS_POINTER/app/src/main/java/com/example/as_pointer/MainActivity.java
+++ AS_POINTER/app/src/main/java/com/example/as_pointer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.as_pointer;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -7,15 +8,18 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity   implements NavigationView.OnNavigationItemSelectedListener{
 
     private long backPressedTime;
+//    final AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
     String backstackname = getFragmentManager().getClass().getName();
 
     @Override
@@ -72,10 +76,43 @@ public class MainActivity extends AppCompatActivity   implements NavigationView.
             } else if (id == R.id.nav_help) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_content, new Help()).addToBackStack(backstackname).commit();
             } else if (id == R.id.nav_logout) {
-                //startActivity(new Intent( MainActivity.this, LoginActivity.class ));
-                Intent intent  = new Intent(this , LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+                // Setting Alert Dialog Title
+                alertDialogBuilder.setTitle("Confirm Exit..!!!");
+                // Icon Of Alert Dialog
+                alertDialogBuilder.setIcon(R.drawable.ic_announcement_black_24dp);
+                // Setting Alert Dialog Message
+                alertDialogBuilder.setMessage("Are you sure, You want to exit!");
+                alertDialogBuilder.setCancelable(false);
+
+                alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        finish();
+                    }
+                });
+
+                alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"You clicked over No",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alertDialogBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"You clicked on Cancel",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
+//                Intent intent = new Intent( this, LoginActivity.class );
+//                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intent);
             } else if (id == R.id.nav_contact) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_content, new ContactUs()).addToBackStack(backstackname).commit();
             }else if (id == R.id.nav_info) {
--- ExpandableCardView-master/README.md
+++
@@ -1,6 +0,0 @@
-# Expandable CardView
-Creating expandable CardView in Android Studio with Java.
-
-See <a href="https://youtu.be/19E85PngufY">tutorial</a>
-
-![Alt Text](https://github.com/gifffert/ExpandableCardView/blob/master/Screenshots/expandableCardView.gif)
